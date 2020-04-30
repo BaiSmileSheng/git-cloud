e9f2694bc5483aaa809bd796a5a16de624cdce25
lenovo <721666450@qq.com>
2020-04-30 20:18:41 +0800

    优化Base方法

--- cloud-common/cloud-common-core/src/main/java/com/cloud/common/core/domain/BaseEntity.java
+++ cloud-common/cloud-common-core/src/main/java/com/cloud/common/core/domain/BaseEntity.java
@@ -6,18 +6,29 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Transient;
 
 /**
  * Entity基类
  *
  * @author cloud
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BaseEntity implements Serializable {
     private static final long serialVersionUID = 1L;
 
     /**
      * 搜索值
      */
+    @JsonIgnore //swagger 不显示字段
+    @Transient  //tk 不操作字段
     private String searchValue;
 
     /**
@@ -47,87 +58,23 @@ public class BaseEntity implements Serializable {
      */
     private String remark;
 
+    @JsonIgnore
+    @Transient
     private String beginTime;
 
+    @JsonIgnore
+    @Transient
     private String endTime;
 
     /**
+     * 删除标志（0代表存在 1代表删除）
+     */
+    private String delFlag;
+    /**
      * 请求参数
      */
+    @JsonIgnore
+    @Transient
     private Map<String, Object> params;
 
-    public String getSearchValue() {
-        return searchValue;
-    }
-
-    public void setSearchValue(String searchValue) {
-        this.searchValue = searchValue;
-    }
-
-    public String getCreateBy() {
-        return createBy;
-    }
-
-    public void setCreateBy(String createBy) {
-        this.createBy = createBy;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public String getUpdateBy() {
-        return updateBy;
-    }
-
-    public void setUpdateBy(String updateBy) {
-        this.updateBy = updateBy;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public String getRemark() {
-        return remark;
-    }
-
-    public void setRemark(String remark) {
-        this.remark = remark;
-    }
-
-    public String getBeginTime() {
-        return beginTime;
-    }
-
-    public void setBeginTime(String beginTime) {
-        this.beginTime = beginTime;
-    }
-
-    public String getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(String endTime) {
-        this.endTime = endTime;
-    }
-
-    public Map<String, Object> getParams() {
-        if (params == null) {
-            params = new HashMap<>();
-        }
-        return params;
-    }
-
-    public void setParams(Map<String, Object> params) {
-        this.params = params;
-    }
 }
