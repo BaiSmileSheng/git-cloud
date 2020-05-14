cb956ad2a463e64e3fbfa88e3f86bc8925ed84c6
lenovo <721666450@qq.com>
2020-05-14 17:22:11 +0800

    settle模块创建

---
+++ cloud-service/cloud-settle/src/main/java/com/cloud/settle/mapper/OmsTestMapper.java
@@ -0,0 +1,15 @@
+package com.cloud.settle.mapper;
+
+
+import com.cloud.common.core.dao.BaseMapper;
+import com.cloud.order.domain.entity.OmsTest;
+
+/**
+ * 订单类测试Mapper接口
+ *
+ * @author cloud
+ * @date 2020-05-02
+ */
+public interface OmsTestMapper extends BaseMapper<OmsTest> {
+
+}
