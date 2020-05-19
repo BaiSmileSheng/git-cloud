951af9f48edcb258cd00a0a42359a367732a62c6
lihongxia <13706494340@163.com>
2020-05-19 19:16:24 +0800

    oss 导包漏合并

--- cloud-service-api/cloud-system-api/src/main/java/com/cloud/system/feign/RemoteOssService.java
+++ cloud-service-api/cloud-system-api/src/main/java/com/cloud/system/feign/RemoteOssService.java
@@ -2,14 +2,13 @@ package com.cloud.system.feign;
 
 import com.cloud.common.constant.ServiceNameConstants;
 import com.cloud.common.core.domain.R;
-import com.cloud.system.domain.entity.SysOss;
 import com.cloud.system.feign.factory.RemoteOssFallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
 
 /**
  * OBS文件管理
