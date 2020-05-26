8fbffdc3011ca2ba1416767ec514b32640350dfd
lenovo <721666450@qq.com>
2020-05-26 18:19:02 +0800

    审批流程标题  全部大写

--- cloud-common/cloud-common-core/src/main/java/com/cloud/common/constant/ActivitiProTitleConstants.java
+++ cloud-common/cloud-common-core/src/main/java/com/cloud/common/constant/ActivitiProTitleConstants.java
@@ -7,5 +7,10 @@ public class ActivitiProTitleConstants {
     /**
      * settle服务相关测试流程
      */
-    public static final String Activiti_PRO_TITLE_SETTLE_TEST = "测试流程申请-settle";
+    public static final String ACTIVITI_PRO_TITLE_SETTLE_TEST = "测试流程申请-settle";
+
+    /**
+     * 物耗管理服务相关流程
+     */
+    public static final String ACTIVITI_PRO_TITLE_SUPPLEMENTARY_TEST = "物耗管理申请流程";
 }
