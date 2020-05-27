898c7b6010a7c141634809b2b0217ef6621040f1
lihongxia <13706494340@163.com>
2020-05-27 13:45:18 +0800

    定时任务计算金额做null判断

--- cloud-job/cloud-job-executor/src/main/java/com/cloud/job/executor/service/jobhandler/SmsSettleInfoCalculateXxlJob.java
+++ cloud-job/cloud-job-executor/src/main/java/com/cloud/job/executor/service/jobhandler/SmsSettleInfoCalculateXxlJob.java
@@ -36,10 +36,12 @@ public class SmsSettleInfoCalculateXxlJob {
         //2.计算加工费 (订单交货数量*sap加工费单价=生产订单加工费金额 )
         //3.修改加工费结算信息表--加工费结算金额,状态为待结算
         for(SmsSettleInfo smsSettleInfo: smsSettleInfoList){
-            BigDecimal settlePrice = smsSettleInfo.getMachiningPrice().multiply(new BigDecimal(smsSettleInfo.getConfirmAmont()));
-            smsSettleInfo.setSettlePrice(settlePrice);
-            smsSettleInfo.setOrderStatus(SettleInfoOrderStatusEnum.ORDER_STATUS_3.getCode());
-            remoteSettleInfoService.editSave(smsSettleInfo);
+            if(null != smsSettleInfo.getMachiningPrice() && null != smsSettleInfo.getConfirmAmont() ){
+                BigDecimal settlePrice = smsSettleInfo.getMachiningPrice().multiply(new BigDecimal(smsSettleInfo.getConfirmAmont()));
+                smsSettleInfo.setSettlePrice(settlePrice);
+                smsSettleInfo.setOrderStatus(SettleInfoOrderStatusEnum.ORDER_STATUS_3.getCode());
+                remoteSettleInfoService.editSave(smsSettleInfo);
+            }
         }
         logger.info("加工费生成结束");
         return ReturnT.SUCCESS;
