6801d32b898870f0bf19056c6d51a0da9a173009
lenovo <721666450@qq.com>
2020-06-01 18:20:15 +0800

    工厂生产线关系表生产代码

---
+++ cloud-service/cloud-system/src/main/java/com/cloud/system/mapper/CdFactoryLineInfoMapper.java
@@ -0,0 +1,13 @@
+package com.cloud.system.mapper;
+
+import com.cloud.system.domain.entity.CdFactoryLineInfo;
+import com.cloud.common.core.dao.BaseMapper;
+/**
+ * 工厂线体关系 Mapper接口
+ *
+ * @author cs
+ * @date 2020-06-01
+ */
+public interface CdFactoryLineInfoMapper extends BaseMapper<CdFactoryLineInfo>{
+
+}
