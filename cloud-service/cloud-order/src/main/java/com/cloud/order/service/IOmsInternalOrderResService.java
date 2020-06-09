277a528dbe09a2d3fa8eb6472e4b82b6032f2e23
A0032554 李同庆 <1342382831@qq.com>
2020-06-09 17:20:39 +0800

    PR/PO数据原表相关文件

---
+++ cloud-service/cloud-order/src/main/java/com/cloud/order/service/IOmsInternalOrderResService.java
@@ -0,0 +1,14 @@
+package com.cloud.order.service;
+
+import com.cloud.common.core.service.BaseService;
+import com.cloud.order.domain.entity.OmsInternalOrderRes;
+
+/**
+ * 内单PR/PO原 Service接口
+ *
+ * @author ltq
+ * @date 2020-06-05
+ */
+public interface IOmsInternalOrderResService extends BaseService<OmsInternalOrderRes>{
+
+    }
