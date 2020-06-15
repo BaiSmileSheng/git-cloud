a096e37f19ccd1ea99531028437951388e4a0cb4
lenovo <721666450@qq.com>
2020-06-15 11:26:29 +0800

    工厂库位表生成代码

---
+++ cloud-service/cloud-system/src/main/java/com/cloud/system/service/ICdFactoryStorehouseInfoService.java
@@ -0,0 +1,14 @@
+package com.cloud.system.service;
+
+import com.cloud.system.domain.entity.CdFactoryStorehouseInfo;
+import com.cloud.common.core.service.BaseService;
+
+/**
+ * 工厂库位 Service接口
+ *
+ * @author cs
+ * @date 2020-06-15
+ */
+public interface ICdFactoryStorehouseInfoService extends BaseService<CdFactoryStorehouseInfo>{
+
+    }
