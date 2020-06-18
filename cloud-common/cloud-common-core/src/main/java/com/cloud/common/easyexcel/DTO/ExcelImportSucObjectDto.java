86b788d929b78c9c9baeef1c7769df909eae781f
lenovo <721666450@qq.com>
2020-06-18 14:02:31 +0800

    Excel导出错误日志

---
+++ cloud-common/cloud-common-core/src/main/java/com/cloud/common/easyexcel/DTO/ExcelImportSucObjectDto.java
@@ -0,0 +1,13 @@
+package com.cloud.common.easyexcel.DTO;
+
+import lombok.Data;
+
+/**
+ * @auther cs
+ * @date 2020/6/18 9:27
+ * @description excel单条数据导入结果 成功
+ */
+@Data
+public class ExcelImportSucObjectDto {
+    private Object object;
+}
