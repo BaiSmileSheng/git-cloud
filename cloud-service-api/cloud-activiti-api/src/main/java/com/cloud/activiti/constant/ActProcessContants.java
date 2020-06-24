d77b134fd4e626b2f478ca82ed5c2d91c0c30ada
A0032554 李同庆 <1342382831@qq.com>
2020-06-24 19:19:54 +0800

    审批流程activiti-api模块文件

---
+++ cloud-service-api/cloud-activiti-api/src/main/java/com/cloud/activiti/constant/ActProcessContants.java
@@ -0,0 +1,47 @@
+package com.cloud.activiti.constant;
+
+public class ActProcessContants {
+
+    /**
+     * ZN认证服务相关流程
+     */
+    public static final String ACTIVITI_PRO_TITLE_ZN = "ZN认证审批流程";
+
+    /**
+     * ZN认证服务相关流程
+     */
+    public static final String ACTIVITI_PRO_TITLE_ADD = "T+2追加订单审批流程";
+    /**
+     * 3版本订单审批流程
+     */
+    public static final String ACTIVITI_PRO_TITLE_THREE_VERSION = "3版本订单审批流程";
+    /**
+     * 超期未关闭订单审批流程
+     */
+    public static final String ACTIVITI_PRO_TITLE_OVERDUE_NOT_CLOSE = "超期未关闭订单审批流程";
+
+    /**
+     * 超期库存订单审批流程
+     */
+    public static final String ACTIVITI_PRO_TITLE_OVERDUE_STOCK = "超期库存订单审批流程";
+
+
+    /**
+     * 审批流程Key
+     * */
+    //ZN认证审批流Key
+    public static final String ACTIVITI_ZN_REVIEW = "productOrderZn";
+
+    //T+2追加订单审批Key
+    public static final String ACTIVITI_ADD_REVIEW = "productOrderAdd";
+
+    //3版本订单审批Key
+    public static final String ACTIVITI_THREE_VERSION_REVIEW = "productOrderThreeVersion";
+
+    //超期未关闭订单审批Key
+    public static final String ACTIVITI_OVERDUE_NOT_CLOSE_ORDER_REVIEW = "productOrderOverNotClose";
+
+    //超期库存订单审批Key
+    public static final String ACTIVITI_OVERDUE_STOCK_ORDER_REVIEW = "productOrderOverStock";
+
+}
