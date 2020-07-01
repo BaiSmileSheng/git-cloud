625c7895817415652089087172c312b30334dd1e
lihongxia <13706494340@163.com>
2020-07-01 11:38:19 +0800

    下达SAP

--- cloud-service/cloud-order/src/main/java/com/cloud/order/service/IOmsProductionOrderService.java
+++ cloud-service/cloud-order/src/main/java/com/cloud/order/service/IOmsProductionOrderService.java
@@ -67,8 +67,26 @@ public interface IOmsProductionOrderService extends BaseService<OmsProductionOrd
     List<OmsProductionOrder> exportAll(OmsProductionOrder omsProductionOrder,SysUser sysUser);
     /**
      * 下达SAP
-     * @param list
+     * @param ids
      * @return
      */
-    R giveSAP(List<OmsProductionOrder> list);
+    R giveSAP(String ids);
+
+    /**
+     * SAP获取订单号
+     */
+    R timeSAPGetProductOrderCode();
+
+    /**
+     * 邮件推送
+     * @return
+     */
+    R mailPush();
+
+    /**
+     * 订单刷新
+     * @param ids
+     * @return
+     */
+    R orderRefresh(String ids);
 }
