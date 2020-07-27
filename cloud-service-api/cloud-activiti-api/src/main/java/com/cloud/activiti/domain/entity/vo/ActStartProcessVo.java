48c3059917c396386cb308cc3317fece652e9e9d
A0032554 李同庆 <1342382831@qq.com>
2020-07-27 14:16:25 +0800

    修改审批流程调用传参

---
+++ cloud-service-api/cloud-activiti-api/src/main/java/com/cloud/activiti/domain/entity/vo/ActStartProcessVo.java
@@ -0,0 +1,24 @@
+package com.cloud.activiti.domain.entity.vo;
+
+import com.alibaba.excel.annotation.ExcelIgnoreUnannotated;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@ExcelIgnoreUnannotated
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ApiModel(value = "审批流开启VO")
+public class ActStartProcessVo {
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "业务ID")
+    private String orderId;
+    @ApiModelProperty(value = "业务Code")
+    private String orderCode;
+}
