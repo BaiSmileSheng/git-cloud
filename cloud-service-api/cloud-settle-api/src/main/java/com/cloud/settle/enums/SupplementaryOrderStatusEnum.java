0fac6943c08f8a7c3fdccd1d718d85faf8e0cd24
lenovo <721666450@qq.com>
2020-08-19 16:02:10 +0800

    报废、物耗状态删除

--- cloud-service-api/cloud-settle-api/src/main/java/com/cloud/settle/enums/SupplementaryOrderStatusEnum.java
+++ cloud-service-api/cloud-settle-api/src/main/java/com/cloud/settle/enums/SupplementaryOrderStatusEnum.java
@@ -13,8 +13,6 @@ public enum SupplementaryOrderStatusEnum {
     WH_ORDER_STATUS_JITBH("2","jit驳回"),
     WH_ORDER_STATUS_XWZDSH("3","小微主待审核"),
     WH_ORDER_STATUS_XWZBH("5","小微主驳回"),
-    WH_ORDER_STATUS_SAPSUCCESS("6","SAP成功"),
-    WH_ORDER_STATUS_SAPFAIL("7","SAP创单失败"),
     WH_ORDER_STATUS_DJS("11","待结算"),
     WH_ORDER_STATUS_JSWC("12","结算完成"),
     WH_ORDER_STATUS_YDX("13","已兑现"),
