30fa85e4776699ff9f14ee7bb4902dcbd2b235ad
lenovo <721666450@qq.com>
2020-09-14 09:24:53 +0800

    13周2周确认下达非本人导入数据不允许下达

--- cloud-service/cloud-order/src/main/java/com/cloud/order/service/impl/OmsDemandOrderGatherEditServiceImpl.java
+++ cloud-service/cloud-order/src/main/java/com/cloud/order/service/impl/OmsDemandOrderGatherEditServiceImpl.java
@@ -187,6 +187,7 @@ public class OmsDemandOrderGatherEditServiceImpl extends BaseServiceImpl<OmsDema
     public R confirmRelease(String ids,OmsDemandOrderGatherEdit omsDemandOrderGatherEditParam,SysUser sysUser) {
         Example example = new Example(OmsDemandOrderGatherEdit.class);
         Example.Criteria criteria = example.createCriteria();
+        String createBy = sysUser.getCreateBy();
         //允许下达的审核状态
         List<String> auditStatusList = CollUtil.newArrayList(DemandOrderGatherEditAuditStatusEnum.DEMAND_ORDER_GATHER_EDIT_AUDIT_STATUS_WXSH.getCode()
                 ,DemandOrderGatherEditAuditStatusEnum.DEMAND_ORDER_GATHER_EDIT_AUDIT_STATUS_SHWC.getCode());
@@ -214,6 +215,7 @@ public class OmsDemandOrderGatherEditServiceImpl extends BaseServiceImpl<OmsDema
             } else {
                 criteria.andIn("auditStatus", auditStatusList);
             }
+            omsDemandOrderGatherEditParam.setCreateBy(createBy);
             listCondition(omsDemandOrderGatherEditParam,criteria);
         }else{
             //查询参数id的数据
@@ -231,6 +233,9 @@ public class OmsDemandOrderGatherEditServiceImpl extends BaseServiceImpl<OmsDema
                 return R.error(StrUtil.format("此状态数据不允许确认下达！需求订单号：{}",omsDemandOrderGatherEdit.getDemandOrderCode()));
             }else if(!CollUtil.contains(auditStatusList,auditStatus)) {
                 return R.error(StrUtil.format("此审核状态数据不允许确认下达！需求订单号：{}",omsDemandOrderGatherEdit.getDemandOrderCode()));
+            }
+            if (!createBy.equals(omsDemandOrderGatherEdit.getCreateBy())) {
+                return R.error(StrUtil.format("此数据非登录用户导入，不可下达！需求订单号：{}",omsDemandOrderGatherEdit.getDemandOrderCode()));
             }
             omsDemandOrderGatherEdit.setStatus(DemandOrderGatherEditStatusEnum.DEMAND_ORDER_GATHER_EDIT_STATUS_DCSAP.getCode());
             omsDemandOrderGatherEdit.setUpdateBy(sysUser.getLoginName());
@@ -271,6 +276,9 @@ public class OmsDemandOrderGatherEditServiceImpl extends BaseServiceImpl<OmsDema
         }
         if (StrUtil.isNotEmpty(omsDemandOrderGatherEdit.getEndTime())) {
             criteria.andLessThanOrEqualTo("deliveryDate", DateUtil.parse(omsDemandOrderGatherEdit.getEndTime()).offset(DateField.DAY_OF_MONTH,1) );
+        }
+        if (StrUtil.isNotEmpty(omsDemandOrderGatherEdit.getCreateBy())) {
+            criteria.andEqualTo("createBy", omsDemandOrderGatherEdit.getCreateBy() );
         }
     }
 
