dbbb5a9e67e60962de92a14ade1b8bde3454e0c4
A0032554 李同庆 <1342382831@qq.com>
2020-09-15 09:55:31 +0800

    排产订单导入增加日志，8310工厂36号线不校验ZN认证信息查询

--- cloud-service/cloud-order/src/main/java/com/cloud/order/service/impl/OmsProductionOrderServiceImpl.java
+++ cloud-service/cloud-order/src/main/java/com/cloud/order/service/impl/OmsProductionOrderServiceImpl.java
@@ -1483,7 +1483,9 @@ public class OmsProductionOrderServiceImpl extends BaseServiceImpl<OmsProduction
                 log.error("根据成品专用号查询物料扩展信息记录为空！");
                 throw new BusinessException("根据成品专用号:"+o.getProductMaterialCode()+",查询物料扩展信息记录为空，请及时维护物料扩展信息数据！");
             }
-            if (StrUtil.isBlank(cdMaterialExtendInfo.getIsZnAttestation())) {
+            if (StrUtil.isBlank(cdMaterialExtendInfo.getIsZnAttestation())
+                    && (!ProductOrderConstants.NEW_FACTORY_CODE.equals(o.getProductFactoryCode())
+                    || !ProductOrderConstants.NEW_LINE_CODE.equals(o.getProductLineCode()))) {
                 log.error("根据成品专用号查询物料扩展信息记录,是否ZN认证信息为空！");
                 throw new BusinessException("根据成品专用号:"+o.getProductMaterialCode()+",查询物料扩展信息记录是否ZN认证信息为空，请及时维护物料扩展信息数据！");
             }
