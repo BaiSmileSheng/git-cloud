d5227b4a753c1615fc10e44ce9a548c544f8c9d7
lenovo <721666450@qq.com>
2020-09-16 10:49:07 +0800

    两周下达SAP返回值

--- cloud-service/cloud-order/src/main/java/com/cloud/order/service/impl/Oms2weeksDemandOrderEditServiceImpl.java
+++ cloud-service/cloud-order/src/main/java/com/cloud/order/service/impl/Oms2weeksDemandOrderEditServiceImpl.java
@@ -1012,10 +1012,10 @@ public class Oms2weeksDemandOrderEditServiceImpl extends BaseServiceImpl<Oms2wee
                         oms2weeksDemandOrderEdit.setStatus(Weeks2DemandOrderEditStatusEnum.DEMAND_ORDER_GATHER_EDIT_STATUS_CSAPYC.getCode());
                     }
                     successList.add(oms2weeksDemandOrderEdit);
-                    String type = outTableOutput.getString("TYPE");
-                    String flag = outTableOutput.getString("FLAG");
-                    String messageOne = StrUtil.format("ABLAD:{},MESSAGE:{};TPYE:{};FLAG:{}"
-                            ,outTableOutput.getString("ABLAD"),outTableOutput.getString("MESSAGE"),type,flag);
+//                    String type = outTableOutput.getString("TYPE");
+//                    String flag = outTableOutput.getString("FLAG");
+                    String messageOne = StrUtil.format("ABLAD:{},MESSAGE:{};"
+                            ,outTableOutput.getString("ABLAD"),outTableOutput.getString("MESSAGE"));
                     sapBuffer.append(messageOne);
                 }
                 sysInterfaceLog.setResults(sapBuffer.toString());
